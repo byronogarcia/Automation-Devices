c072f0affb522e08b6f690c4f9aa146ab9d87eab
Byron Garcia <byrongarcia48@yahoo.com>
2023-06-14 01:44:09 -0700

    Setting up python file

---
+++ testMeasure.py
@@ -0,0 +1,8 @@
+import sys
+import socket
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+from PyQt5.QtChart import QChart, QChartView, QLineSeries
+from PyQt5.QtPrintSupport import QPrinter
+from PyQt5 import QtCore
